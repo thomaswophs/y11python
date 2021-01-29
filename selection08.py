23ed05f730b1e2bc1c42045f3c8cb33b7c5cd65e
thomaswophs <71124229+thomaswophs@users.noreply.github.com>
2021-01-29 10:41:44 +0000

    Create selection08.py

---
+++ selection08.py
@@ -0,0 +1,9 @@
+num=int(input("plase give the number 1 2 or 3"))
+if num == 1:
+    print("thank you")
+elif num == 2:
+    print("well done you're so smart")
+elif num == 3:
+    print("amazing you're so good at this")
+else:
+    print("what is wrong with you")
