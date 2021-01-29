236b460b820f987042ee76bde489f1b0a916de49
thomaswophs <71124229+thomaswophs@users.noreply.github.com>
2021-01-29 09:32:37 +0000

    Delete subprograms03.py

--- subprograms03.py
+++
@@ -1,6 +0,0 @@
-name1=(input("you`re invited to"))
-name2=(input("`s birthday party st the"))
-def bIRTH(ahh,noo):
-  print("residence")
-  print("happy BIRTH to you happy BIRTH to you happy BIRTH dear",name1,name2,"HAPPY BIRTHDAY TO YOUUUU~~~")
-bIRTH(name1, name2)
