bdef8173640dbacb34df563e26a910328fdb5eee
thomaswophs <71124229+thomaswophs@users.noreply.github.com>
2021-01-29 10:20:07 +0000

    Create selection04.py

---
+++ selection04.py
@@ -0,0 +1,6 @@
+colour=(input("whats your favorite colour"))
+
+if colour == "PINK" or colour == "Pink" or colour == "pink":
+    print("i like pink too")
+else:
+    print("i hate",colour,",",colour,"is for losers")
