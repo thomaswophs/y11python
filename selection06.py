53ec42f3cd8081b3095ba331c795584b49b3dc22
thomaswophs <71124229+thomaswophs@users.noreply.github.com>
2021-01-29 10:32:04 +0000

    Create selection06.py

---
+++ selection06.py
@@ -0,0 +1,9 @@
+age=int(input("age?"))
+if age >= 18:
+    print("you can vote")
+elif age == 17:
+    print("you can learn to drive")
+elif age == 16:
+    print("you can buy a lottery ticcket")
+else:
+    print("you can trick or treat")
