0ffffa216493361a0f0dc1577ec776dfa71b090c
thomaswophs <71124229+thomaswophs@users.noreply.github.com>
2021-04-23 15:28:07 +0100

    Update selection04.py

--- selection04.py
+++ selection04.py
@@ -4,3 +4,4 @@ if colour == "PINK" or colour == "Pink" or colour == "pink":
     print("i like pink too")
 else:
     print("i hate",colour,",",colour,"is for losers")
+# made by thomas selection 04
