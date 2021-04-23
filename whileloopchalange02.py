8acb75d3072ea5e35bee9799ec3f4ac7bd1cfee6
thomaswophs <71124229+thomaswophs@users.noreply.github.com>
2021-04-23 15:30:19 +0100

    Update whileloopchalange02.py

--- whileloopchalange02.py
+++ whileloopchalange02.py
@@ -3,3 +3,4 @@ while total <= 5:
     newnum=int(input("please enter a number"))
     total=total+newnum
 print("the last number you entered was a",newnum,".")
+# made by thomas while loop challange 02
